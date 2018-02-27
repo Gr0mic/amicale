6770307bda054d44d55b9f2dacf3720437aa3e59
Gromic <morreelsm@gmail.com>
2018-02-27 19:59:12 +0100

    use laravel collective

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -4,6 +4,13 @@
     <div class="container-fluid">
         <h2>Users create</h2>
 
+        {!! Form::open(['route' => 'user.store']) !!}
+
+        {!! Form::text('lastname', 'Nom') !!}
+        {!! Form::text('firstname', 'Prénom') !!}
+        {!! Form::email('email', 'jhon@doo.coo') !!}
+
+        {!! Form::close() !!}
 
     </div>
 
